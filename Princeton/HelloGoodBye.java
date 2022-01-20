467d1ad7bbdb25eac04ca29a7affd1ffcdf64601
Jeremy Ward <exochos@gmail.com>
2022-01-19 16:35:09 -0800

    Create HelloGoodBye.java

---
+++ Princeton/HelloGoodBye.java
@@ -0,0 +1,12 @@
+// Princeton DataStructures and Algorithms
+// Winter 2022
+// Jeremy Ward
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        String firstValue = args[0];
+        String secondValue = args[1];
+        System.out.println("Hello " + firstValue + " and " + secondValue + ".");
+        System.out.println("Goodbye " + secondValue + " and " + firstValue + ".");
+    }
+}
